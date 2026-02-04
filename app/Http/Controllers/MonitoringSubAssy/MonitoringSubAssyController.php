26e21942b6c72f7f05354ad4d2a2e6c1217de0e5
teguhraharjo17 <teguhraharjorubiyo27@gmail.com>
2026-02-04 15:07:45 +0700

    filterandsortcustomer monitoring SA

--- app/Http/Controllers/MonitoringSubAssy/MonitoringSubAssyController.php
+++ app/Http/Controllers/MonitoringSubAssy/MonitoringSubAssyController.php
@@ -54,7 +54,17 @@ class MonitoringSubAssyController extends Controller
             }
         }
 
-        $rekapData = RekapData::where('bulan', $bulan)->where('tahun', $tahun)->get();
+        $customer = $request->customer;
+
+        $rekapData = RekapData::where('bulan', $bulan)
+            ->where('tahun', $tahun)
+            ->when($customer, function ($q) use ($customer) {
+                $q->where('customer', $customer);
+            })
+            ->orderBy('customer')
+            ->orderBy('kode_project')
+            ->orderBy('part_number')
+            ->get();
 
         $subAssies = SubAssy::with('details')
             ->where('bulan', $bulan)
